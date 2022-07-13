7e7ac5649249b075d2ff84783a5aaf78b729d5c6
HANYUNSEONG <hanyunseong.dev@gmail.com>
2022-07-14 00:23:18 +0900

    feat: TodoList 컴포넌트 추가

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,25 @@
+import TodoItem from "components/TodoItem";
+import { Todo } from "types/todo";
+
+type Props = {
+  todos: Array<Todo>;
+  onToggle: (id: number) => void;
+  onRemove: (id: number) => void;
+};
+
+function TodoList({ todos, onToggle, onRemove }: Props) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
