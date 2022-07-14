5891e6da3f9bbcf81f98f122e9af2f7c24d5c617
HANYUNSEONG <hanyunseong.dev@gmail.com>
2022-07-15 01:26:01 +0900

    feat: TodoApp setTodos 로직 추가

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -1,11 +1,41 @@
 import TodoForm from "components/TodoForm";
 import TodoList from "components/TodoList";
+import { useCallback, useRef, useState } from "react";
+import { Todo } from "types/todo";
 
 function TodoApp() {
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: 1,
+      content: "운동하기",
+      done: false,
+    },
+    {
+      id: 2,
+      content: "개발 공부하기",
+      done: false,
+    },
+  ]);
+  const nextId = useRef<number>(3);
+
+  const onTodoFormSubmit = useCallback((content: string) => {
+    setTodos((prevTodos) => {
+      const newTodo = {
+        id: nextId.current,
+        content,
+        done: false,
+      };
+
+      return prevTodos.concat(newTodo);
+    });
+
+    nextId.current += 1;
+  }, []);
+
   return (
     <>
-      <TodoForm data-testId="helloworld" onSubmit={() => {}} />
-      <TodoList todos={[]} onToggle={() => {}} onRemove={() => {}} />
+      <TodoForm data-testId="helloworld" onSubmit={onTodoFormSubmit} />
+      <TodoList todos={todos} onToggle={() => {}} onRemove={() => {}} />
     </>
   );
 }
